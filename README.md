dc0027002fb474e451da462a46589ba9bd2cca2c
Aditya <aditya.sharma263@gmail.com>
2018-06-06 12:25:09 +0530

    boilerplate created

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# compare_travels
-compare the top travel website's price 
+#=======#
+**COMPARE TRAVELS**
+#=======#
+
+compare the top travel website's price
+
+## **Server-side**
+* [Python 3.5+](http://www.python.org): The language of choice.
+
+#### create virtual environment
+    virtualenv -p python3 venv
+    
+#### activate virtual environment 
+    source venv/bin/activate
+    
+#### install requirements
+    pip3 install -r requirements/default.txt
+    
+#### upgrade migration
+    python manage.py db upgrade
+    
+#### make new migartion for changes in schema
+    python manage.py db migrate
+
+#### for run the local server 
+    python manage.py runserver
+    
+
---
+++ manage.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from books import db, app
+# from books.seed import SeedData
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+# manager.add_command('seed', SeedData)
+manager.add_command("runserver", Server(use_debugger=app.config['SERVER_DEBUG'],
+                                        use_reloader=app.config['SERVER_RELOAD'],
+                                        host=app.config['SERVER_HOST'],
+                                        port=int(app.config['SERVER_PORT']),
+                                        threaded=True))
+if __name__ == "__main__":
+    manager.run()
