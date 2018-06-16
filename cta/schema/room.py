25fd288b6af474fb1ad5d32c8b99d568ae7d349b
Aditya <aditya.sharma263@gmail.com>
2018-06-16 21:04:57 +0530

    date filter

--- cta/schema/room.py
+++ cta/schema/room.py
@@ -4,6 +4,7 @@ from cta.model.room import Room
 from cta.model.room import Facility
 from cta.model.room import Member
 from cta import ma
+from cta.schema.base import safe_execute
 
 
 class MemberSchema(ma.ModelSchema):
@@ -21,6 +22,15 @@ class FacilitySchema(ma.ModelSchema):
 class RoomSchema(ma.ModelSchema):
     member = ma.Nested(MemberSchema, many=False)
     facilities = ma.Nested(FacilitySchema, many=False)
+    check_in = ma.Method('check_in_epoch')
+    check_out = ma.Method('check_out_epoch')
+
+    def check_in_epoch(self, obj):
+        return safe_execute(None, ValueError, obj.check_in)
+
+    def check_out_epoch(self, obj):
+        return safe_execute(None, ValueError, obj.check_out)
+
     class Meta:
         model = Room
         exclude = ('updated_at', 'created_at')
