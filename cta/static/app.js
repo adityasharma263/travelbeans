bb67e066458eca02483ee58cf10bc5947d2d81fc
priyanka <priyanka.goel1997@gmail.com>
2018-06-23 20:43:48 +0530

    admin changes

--- cta/static/app.js
+++ cta/static/app.js
@@ -87,6 +87,8 @@ angular.module('comparetravel', ['angular.filter'])
   $scope.hotel = {};
   $scope.hotelImg = []; //for all images in the hotel
   $scope.ImagesUrl=[];//for one image
+  $scope.deals=[];
+  $scope.hotelDeals = [];
 
   var sendPostCall = function(url, data) {
     console.log(data);
@@ -145,6 +147,14 @@ angular.module('comparetravel', ['angular.filter'])
     $scope.hotelImg.push($scope.ImagesUrl);
     $scope.ImagesUrl="";
     console.log("after push",$scope.hotelImg);
+
+  }
+
+  $scope.addPrice=function(){
+    console.log("before push",$scope.deals);
+    $scope.hotelDeals.push($scope.deals);
+    delete $scope.deals;
+    console.log("after push",$scope.hotelDeals);
 
   }
   
