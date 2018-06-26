fc89642df865d7cb1f901ad97807ba80e7a02335
Aditya <aditya.sharma263@gmail.com>
2018-06-26 16:54:11 +0530

    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'), unique=True)

---
+++ cta/model/restaurant.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from cta import db
+from cta.model.base import Base
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+
+    name = db.Column(db.String)
+    rating = db.Column(db.Float, nullable=True)
+    city = db.Column(db.String, nullable=True)
+    phone = db.Column(db.String, nullable=True, unique=True)
+    latitude = db.Column('latitude', db.Float(asdecimal=True), nullable=True)
+    longitude = db.Column('longitude', db.Float(asdecimal=True), nullable=True)
+    address = db.Column(db.String, nullable=True)
+    images = db.relationship('Image', backref='hotel')
+    amenities = db.relationship('Amenity', uselist=False, backref='hotel')
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<name %r>' % self.name
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    image_url = db.Column(db.String, default=False, nullable=True)
+    image_type = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<image_url %r>' % self.image_url
+
+class Amenity(Base):
+    __tablename__ = 'amenity'
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), unique=True)
+    conference_room = db.Column(db.Boolean, default=False, nullable=True)
+    express_check_in_out = db.Column(db.Boolean, default=False, nullable=True)
+    laundry_service = db.Column(db.Boolean, default=False, nullable=True)
+    indoor_swimming_pool = db.Column(db.Boolean, default=False, nullable=True)
+    outdoor_swimming_pool = db.Column(db.Boolean, default=False, nullable=True)
+    porter_service = db.Column(db.Boolean, default=False, nullable=True)
+    Room_cleaning_service = db.Column(db.Boolean, default=False, nullable=True)
+    terrace = db.Column(db.Boolean, default=False, nullable=True)
+    child_baby_cot = db.Column(db.Boolean, default=False, nullable=True)
+    wheelchair_accessible = db.Column(db.Boolean, default=False, nullable=True)
+    doorman = db.Column(db.Boolean, default=False, nullable=True)
+    hairdresser = db.Column(db.Boolean, default=False, nullable=True)
+    banquets = db.Column(db.Boolean, default=False, nullable=True)
+    non_smoking_smoking_rooms = db.Column(db.Boolean, default=False, nullable=True)
+    pet_allowance = db.Column(db.Boolean, default=False, nullable=True)
+    lift = db.Column(db.Boolean, default=False, nullable=True)
+    bar = db.Column(db.Boolean, default=False, nullable=True)
+    gym = db.Column(db.Boolean, default=False, nullable=True)
+    pool = db.Column(db.Boolean, default=False, nullable=True)
+    restaurant = db.Column(db.Boolean, default=False, nullable=True)
+    spa = db.Column(db.Boolean, default=False, nullable=True)
+    wifi_in_lobby = db.Column(db.Boolean, default=False, nullable=True)
+    twenty_four_hr_reception = db.Column(db.Boolean, default=False, nullable=True)
+    twenty_four_hr_room_service = db.Column(db.Boolean, default=False, nullable=True)
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<pool %r>' % self.pool
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    image_url = db.Column(db.String, default=False, nullable=True)
+    image_type = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<image_url %r>' % self.image_url
