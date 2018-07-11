c039a8b4aeb242b3d22e513406ecfcb091ca0017
Abhishek <sharmaabhi069.as@gmail.com>
2018-07-11 17:53:32 +0530

    abhishek

--- cta/static/app.js
+++ cta/static/app.js
@@ -508,22 +508,40 @@ var getrooms=function(){
       // or server returns response with an error status.
   });
 }
-
+var i=0;
 $( ".flex-next" ).click(function() {
-
-  if (($( ".demo" ).css( "transform","translate3d(0px, 0px, 0px)")) && ($scope.roomData.hotelData.images.length >= 10) ){
+  if (($( ".demo" ).css( "transform","translate3d(0px, 0px, 0px)")) && ($scope.roomData.hotelData.images.length >= 10) && (i==0)){
 
     $(".demo").css("transform","translate3d(-791px, 0px, 0px)");
+    i++;
+    return i;
   } 
-  else if($( ".demo" ).css( "transform","translate3d(-791px, 0px, 0px)") ) {
+
+  if (($( ".demo" ).css( "transform","translate3d(-791px, 0px, 0px)")) && ($scope.roomData.hotelData.images.length >= 20) && (i==1)){
+
+    $(".demo").css("transform","translate3d(-1582px, 0px, 0px)");
+    i++;
+    return i;
+  } 
+  else {
     $(".demo").css("transform","translate3d(0px, 0px, 0px)" );
+    i=0;
+    return i;
   }
 });
 
 $( ".flex-prev" ).click(function() {
 
-  if ($( ".demo" ).css( "transform","translate3d(-791px, 0px, 0px)") ){
+  if ($( ".demo" ).css( "transform","translate3d(-1582px, 0px, 0px)") && (i==2)){
+    $(".demo").css("transform","translate3d(-791px, 0px, 0px)");
+    i--;
+    return i;
+  } 
+
+  if ($( ".demo" ).css( "transform","translate3d(-791px, 0px, 0px)") && (i==1)){
     $(".demo").css("transform","translate3d(0px, 0px, 0px)");
+    i--;
+    return i;
   } 
   else {
     $(".demo").css("transform","translate3d(0px, 0px, 0px)" );
