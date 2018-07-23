66a021ea303b63c80be749fd38f5200fb607c858
Aditya <aditya.sharma263@gmail.com>
2018-07-23 13:01:55 +0530

    modified for server

--- cta/view/hotel.py
+++ cta/view/hotel.py
@@ -418,8 +418,8 @@ def deal_api():
             price = Deal.query.filter_by(**args).all()
         result = DealSchema(many=True).dump(price)
         for deal in result.data:
-            if deal["room"]:
-                deal["hotel_id"] = Room.query.filter(Room.id == deal["room"]).first().hotel_id
+            # if deal["room"]:
+            #     deal["hotel_id"] = Room.query.filter(Room.id == deal["room"]).first().hotel_id
             if no_of_days >= 1 and deal['price']:
                 deal['price'] = int(deal["price"]) * no_of_days
         return jsonify({'result': {'deal': result.data}, 'message': "Success", 'error': False})
