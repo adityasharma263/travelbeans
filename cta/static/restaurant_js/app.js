395f33d87254a707c0d835610cf0aa8c4ca7b609
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2018-07-24 09:13:02 +0530

    add menu filter

--- cta/static/restaurant_js/app.js
+++ cta/static/restaurant_js/app.js
@@ -21,19 +21,24 @@ var app = angular.module("restaurantApp", ['angular.filter'])
   }])
   .controller("searchController", ["$scope", "$http", function ($scope, $http) {
 
+    var restaurantStructure = {}
+
     $http.get("/api/v1/restaurant/cuisine")
       .then(function (res) {
         $scope.cuisine = res.data.result.cuisine;
-        console.log($scope.cuisine);
       }, function (err) {
         console.log(err);
       });
 
 
-      $http.get("/api/v1/restaurant")
+    $http.get("/api/v1/restaurant")
       .then(function (res) {
         $scope.restaurants = res.data.result.restaurants;
-        console.log("$scope.resturants =",$scope.restaurants);
+        var allRestaurants = $scope.restaurants;
+        for (i in allRestaurants) {
+          restaurantStructure[allRestaurants[i].id] = allRestaurants[i];
+        };
+        
       }, function (err) {
         console.log(err);
       });
@@ -42,7 +47,6 @@ var app = angular.module("restaurantApp", ['angular.filter'])
     $http.get("/api/v1/restaurant/collection")
       .then(function (res) {
         $scope.collection = res.data.result.collection;
-        console.log($scope.collection);
       }, function (err) {
         console.log(err);
       });
@@ -50,7 +54,6 @@ var app = angular.module("restaurantApp", ['angular.filter'])
     $http.get("/api/v1/restaurant/dish")
       .then(function (res) {
         $scope.dish = res.data.result.dish;
-        console.log($scope.dish);
       }, function (err) {
         console.log(err);
       });
@@ -59,42 +62,75 @@ var app = angular.module("restaurantApp", ['angular.filter'])
     $http.get("/api/v1/restaurant/association")
       .then(function (res) {
         $scope.association = res.data.result.association;
-        console.log($scope.association);
       }, function (err) {
         console.log(err);
       });
-
-
-    // $http.get("/api/v1/restaurant/tag")
-    //   .then(function (res) {
-    //     $scope.tag = res.data.result.tag;
-    //     console.log($scope.tag);
-    //   }, function (err) {
-    //     console.log(err);
-    //   });
 
     $http.get("/api/v1/restaurant/amenity")
       .then(function (res) {
-        $scope.amenities = res.data.result.amenities;
-        console.log($scope.amenities);
+        $scope.amenities = res.data.result.amenities[0];
       }, function (err) {
         console.log(err);
       });
 
 
-
-    $scope.getFilterRestaurant = function(filterName, filterValue){
-
-      var filter = {
-
-      }
-      filter[filterName] = filterValue;
-      $http.get("/api/v1/restaurant", {params:filter})
+    $http.get("/api/v1/restaurant/tag")
       .then(function (res) {
-        $scope.restaurants = res.data.result.restaurants;
+        $scope.tag = res.data.result.tag[0];
+        console.log("$scope.tag =",$scope.tag);
       }, function (err) {
         console.log(err);
       });
+
+    $http.get("/api/v1/restaurant/amenity")
+      .then(function (res) {
+        $scope.amenities = res.data.result.amenities[0];
+        delete $scope.amenities.id;
+        delete $scope.amenities.restaurant;
+      }, function (err) {
+        console.log(err);
+      });
+
+
+
+    $scope.getFilterRestaurant = function (filterName, filterValue) {
+
+      var filter = {}
+      filter[filterName] = filterValue;
+      $http.get("/api/v1/restaurant", { params: filter })
+        .then(function (res) {
+          $scope.restaurants = res.data.result.restaurants;
+        }, function (err) {
+          console.log(err);
+        });
+
+    }
+
+    $scope.amenityAndTagFilter = function (filterType, filterValue) {
+
+      var filter = {}
+      filter[filterValue] = true;
+
+      var filterTypeResponse={
+        tag : "tag",
+        amenity : "amenities"
+      }
+
+      $http.get("/api/v1/restaurant/"+filterType, { params: filter })
+        .then(function (res) {
+          var amenitiesAndTagFilterResponse = res.data.result[filterTypeResponse[filterType]];
+          
+          var restaurant = [];
+          for (i in amenitiesAndTagFilterResponse) {
+            restaurant.push(restaurantStructure[amenitiesAndTagFilterResponse[i].restaurant])
+          }
+
+          $scope.restaurants = restaurant;
+
+        }, function (err) {
+          console.log(err);
+        });
+
 
     }
 
