f36d254d36787f296b2944fb1866e0ebad01dd57
Aditya <aditya.sharma263@gmail.com>
2018-07-29 18:33:00 +0530

    website add

--- cta/schema/cab.py
+++ cta/schema/cab.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from cta.model.cab import Cab, CabAmenity, CabBooking, CabImage, CabInvoice, CabTax
+from cta.model.cab import Cab, CabAmenity, CabBooking, CabImage, CabInvoice, CabTax, CabWebsite
 from cta import ma
+
+
+class CabWebsiteSchema(ma.ModelSchema):
+    class Meta:
+        model = CabWebsite
+        exclude = ('updated_at', 'created_at')
 
 
 class CabTaxSchema(ma.ModelSchema):
@@ -23,6 +29,8 @@ class CabImageSchema(ma.ModelSchema):
 
 
 class CabInvoiceSchema(ma.ModelSchema):
+    websites = ma.Nested(CabWebsiteSchema, many=False)
+
     class Meta:
         model = CabInvoice
         exclude = ('updated_at', 'created_at')
@@ -30,13 +38,14 @@ class CabInvoiceSchema(ma.ModelSchema):
 
 class CabBookingSchema(ma.ModelSchema):
     invoices = ma.Nested(CabInvoiceSchema, many=False)
+
     class Meta:
         model = CabBooking
         exclude = ('updated_at', 'created_at')
 
 
 class CabSchema(ma.ModelSchema):
-    booking = ma.Nested(CabBookingSchema, many=True)
+    bookings = ma.Nested(CabBookingSchema, many=True)
     amenities = ma.Nested(CabAmenitySchema, many=False)
     images = ma.Nested(CabImageSchema, many=True)
 
