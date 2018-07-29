b882069fe81e3a7dbfcb34827235d7c13e76ae7a
ShivaniRastogi <shivanirastogi1997@gmail.com>
2018-07-29 17:57:56 +0530

    cab detail

--- cta/static/cab_js/app.js
+++ cta/static/cab_js/app.js
@@ -34,3 +34,24 @@ angular.module('comparetravel', ['angular.filter'])
 
 
   }]) 
+
+  .controller('detailController',["$scope", "$http", function($scope, $http, $filter) {
+    $scope.cab= {}; // main cab model
+    console.log("js file");
+    $http({
+        method: 'GET',
+        url: '/api/v1/cab' 
+      }).then(function successCallback(response) {
+          
+          $scope.cab= response.data.result.cabs;
+          console.log("cab data",$scope.cab);
+          // this callback will be called asynchronously
+          // when the response is available
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+      })
+   
+
+
+  }]) 
